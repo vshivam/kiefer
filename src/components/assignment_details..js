60970db479bc418f92a91c8da674cfc7622023cb
Shivam Verma <shivam.bitsgoa@gmail.com>
2019-12-11 22:30:01 +0100

    vote request

--- src/components/assignment_details..js
+++ src/components/assignment_details..js
@@ -23,8 +23,8 @@ import {
 class AssignmentDetails extends React.Component {
   state = {
     submissionSuccessful: false,
-    submissions: [],
-    alreadySubmitted: false
+    submissions: [{ text: "yo" }],
+    alreadySubmitted: true
   };
 
   componentDidMount = () => {
@@ -74,7 +74,10 @@ class AssignmentDetails extends React.Component {
     this.getAllPostsRequest().then(result => {
       if (result.data.success) {
         console.log(result.data.posts);
-        this.setState({ submissions: result.data.posts });
+        this.setState({
+          submissions: result.data.posts,
+          alreadySubmitted: result.data.submitted
+        });
       }
     });
   };
@@ -84,48 +87,81 @@ class AssignmentDetails extends React.Component {
       <Card fluid>
         <Card.Content>{submission.text}</Card.Content>
         <Card.Description>
-          {submission.amazing_story} <Icon name="star outline" color="yellow" size="large" />
-          {submission.well_written} <Icon name="handshake outline" color="blue" size="large" />
-          {submission.hilarious} <Icon name="hand peace outline" color="purple" size="large" />
+          {submission.amazing_story}{" "}
+          <Icon name="star outline" color="yellow" size="large" onClick={() => this.vote("amazing_story", submission.mongoid)}/>
+          {submission.well_written}{" "}
+          <Icon name="handshake outline" color="blue" size="large"  onClick={() => this.vote("well_written", submission.mongoid)}/>
+          {submission.hilarious}{" "}
+          <Icon name="hand peace outline" color="purple" size="large"  onClick={() => this.vote("hilarious", submission.mongoid)}/>
         </Card.Description>
-        <Card.Description>
-        </Card.Description>
+        <Card.Description></Card.Description>
         <Card.Meta></Card.Meta>
       </Card>
     );
   };
 
-  renderFriendSubmissions = (submissions) => {
-    return submissions.map(s => {
-        return this.renderFriendSubmission(s);
-    })
-  }
+  renderFriendSubmissions = submissions => {
+    return (
+      <Fragment>
+        <Header> Your classmates said:</Header>
+        {submissions.map(s => {
+          return this.renderFriendSubmission(s);
+        })}
+      </Fragment>
+    );
+  };
 
   renderSubmissionForm = () => {
-    return this.state.submissionSuccessful ? (
-        <Message success> Well done!</Message>
-      ) : (
-        <Form onSubmit={this.onCreateSubmission}>
-          <Header>{this.props.location.assignment.header}</Header>
-          <Form.Field>
-            <label>Give it your best shot!</label>
-            <TextArea name="submission_text" onChange={this.onFormChange} />
-          </Form.Field>
-          <Message>
-            Don't forget to include these words for extra points ;)
-            <b>{this.props.location.assignment.keyword}</b>
-          </Message>
-          <Form.Button type="submit"> Submit</Form.Button>
-        </Form>
-      );
+    return this.state.submissionSuccessful || this.state.alreadySubmitted ? (
+      <Message success>
+        {" "}
+        Well done on finishing this assignment! Now check out posts from your
+        friends and see what else you can learn.
+      </Message>
+    ) : (
+      <Form onSubmit={this.onCreateSubmission}>
+        <Header>{this.props.location.assignment.header}</Header>
+        <Form.Field>
+          <label>Give it your best shot!</label>
+          <TextArea name="submission_text" onChange={this.onFormChange} />
+        </Form.Field>
+        <Message>
+          Don't forget to include these words for extra points ;)
+          <b>{this.props.location.assignment.keyword}</b>
+        </Message>
+        <Form.Button type="submit"> Submit</Form.Button>
+      </Form>
+    );
+  };
+
+  vote = (type, post_id) => {
+      this.getVoteRequest(type, post_id).then(result => {
+          if(result.data.success){
+            this.getAllSubmissions();      
+          }
+      })
   }
+
+  getVoteRequest = (type, post_id) => {
+      let params = {}
+      params[type]= true
+      params[post_id] = post_id
+    return Axios({
+      method: "get",
+      url: "http://127.0.0.1:8000" + "/user/reaction",
+      params: params,
+      withCredentials: true
+    });
+  };
 
   render = () => {
     console.log(this.props.location);
-    return <Fragment>
+    return (
+      <Fragment>
         {this.renderSubmissionForm()}
         {this.renderFriendSubmissions(this.state.submissions)}
-    </Fragment>
+      </Fragment>
+    );
   };
 }
 
